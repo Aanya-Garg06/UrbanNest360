d5aea0dc4be44def66dc7e9f33721a9a351d01f0
aanya garg <aanya0085.becse24@chitkara.edu.in>
2026-05-18 20:56:22 +0530

    changed server.js

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ app.use('/uploads', express.static(uploadsDir));
 app.use((req, res, next) => { console.log(`${req.method} ${req.url}`); next(); });
 
 app.use(session({
-  secret: 'urban-nest-360-secret-key-2026',
+  secret: 'process.env.SESSION_SECRET',
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
